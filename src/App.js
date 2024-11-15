30f10800423f25a351e7f8a20b304e403344bd01
jordin <jordinmathew.mec@gmail.com>
2024-11-15 21:55:08 +1000

    updating database

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
 
 function App() {
-  const showCart=useSelector(state=>state.ui.showCart);
+  const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://test-9aacd-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
   return (
     <Layout>
-     {showCart && <Cart />} 
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
